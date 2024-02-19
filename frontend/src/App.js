d0c3746ab845f38eafa791b9296ce0131b956bd5
Elemental-Draco <jdwashington31@gmail.com>
2024-02-18 20:33:43 -0500

    changed pages and components to use jsx, added bootstrap as well. started working on login page and functionality

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 // pages and components
 import Home from "./pages/Home";
+import Login from "./pages/Login";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <div className="pages">
           <Routes>
             <Route path="/" element={<Home />} />
+            <Route path="/login" element={<Login />} />
           </Routes>
         </div>
       </BrowserRouter>
---
+++ frontend/src/pages/Login.jsx
@@ -0,0 +1,12 @@
+const Login = () => {
+  return (
+    <div className="login-page">
+      <h2>Login</h2>
+      <form action="/login" method="post">
+        <div class="btn"> hello</div>
+      </form>
+    </div>
+  );
+};
+
+export default Login;
