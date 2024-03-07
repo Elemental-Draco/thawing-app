97d3224b5f3a4c69cb82004a9a6f0922a58d6025
Elemental-Draco <jdwashington31@gmail.com>
2024-03-07 10:23:36 -0500

    commiting to allow merge

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -3,7 +3,8 @@ const Login = () => {
     <div className="login-page">
       <h2>Login</h2>
       <form action="/login" method="post">
-        <div class="btn"> hello</div>
+        <div class="form-control input" placeholder="Username"></div>
+        <div class="form-control input" placeholder="Password" value=""></div>
       </form>
     </div>
   );
